4c9bb02408825a1f92054214506aa1ac9d81c477
haJy <65567064+haJye@users.noreply.github.com>
2022-03-14 15:05:11 +0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,19 @@
+//Swap with algorithm
+let a=-5;
+let b=6;
+a=a+b;
+b=a-b;
+a=a-b;
+console.log(a);
+console.log(b);
+//Swap function
+let c=10;
+let d=11;
+console.log([c,d] = [d,c]);
+//3 variable swap
+let one = 15;
+let two = 16;
+let temp = one;
+one=two;
+two=temp;
+console.log(one, two);
